8698e46c712272689c0c92f4b16f05d3a149ed63
Camilo Correa <juancamilocorrea@hotmail.com>
2026-06-18 20:01:44 -0400

    fix: add missing jsonify import to articles routes

--- routes/articles.py
+++ routes/articles.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_login import login_required, current_user
 from models import db
 from models.article import Article, ArticleReaction
